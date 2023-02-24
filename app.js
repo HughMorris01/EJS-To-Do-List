1d3d2f532e6682653fba15497a7f51312e04b6d0
HughMorris01 <gfarrell2@Live.com>
2023-02-24 13:52:25 -0500

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,81 +1,60 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
+const date = require(__dirname + "/date.js");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+console.log(date);
 
 const app = express();
 
-app.set('view engine', 'ejs');
+const items = ["Write Code", "Hit the Gym",  "Test Code", "Walk the Dog", "Refactor Code"];
+const workItems = ["Write Code", "Test Code", "Refactor Code"];
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://gfarrell82:GQueen4383@cluster0.4qohyx8.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
+app.get("/", function (req, res) {
 
-const postSchema = {
-  title: String,
-  content: String
-};
+    const day = date.getDate();
 
-const Post = mongoose.model("Post", postSchema);
-
-app.get("/", function(req, res){
-
-  Post.find({}, function(err, posts){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
+    res.render("list", { 
+        listTitle: day,
+        newListItems: items 
     });
-  });
-});
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
 });
 
 
+app.post("/", function(req, res){
+    const item = req.body.newItem;
 
-app.post("/compose", function(req, res){
-  const post = new Post ({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-  post.save(function(err){
-    if(!err) {
-      res.redirect("/");
+    if(req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work")
     }
-  });
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
-app.get("/posts/:postId", function(req, res){
 
-  const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-        res.render("post", {
-          title: post.title,
-          content: post.content
-        });
+app.get("/work", function(req, res){
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
     })
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+
+app.post("/work", function(req, res){
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
 });
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("App listening on port 3000.");
 });
